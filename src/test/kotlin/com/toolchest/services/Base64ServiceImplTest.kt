f13860fbaa363af93e178ee01bec4b693e189bbc
Philippe Tillheden <>
2025-04-14 23:28:58 +0200

    qq

--- src/test/kotlin/com/toolchest/services/Base64ServiceImplTest.kt
+++ src/test/kotlin/com/toolchest/services/Base64ServiceImplTest.kt
@@ -24,7 +24,7 @@ class Base64ServiceImplTest {
         
         @Test
         fun `encoding a standard string should produce the correct Base64 value`() = runTestWithSetup {
-            // Assign a real implementation directly
+            // Assign
             val service = Base64ServiceImpl()
             
             // Execute test
